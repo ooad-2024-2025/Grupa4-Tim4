2f3735970a73d42a3be048e0f23caa0f40d2c26d
tbabahmeto1 <tbabahmeto1@etf.unsa.ba>
2025-04-15 15:38:19 +0200

    Pocetni projekat

---
+++ eOpcina/eOpcina/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eOpcina.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
